7845860e16ae207f3c0ddc0ef745e9f9a0892c31
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-02-07 14:54:45 +0530

    authentication controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,1 +1,18 @@
-// File for login, signup, logout, re-set password.
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+export async function Login(){
+
+}
+
+export async function SignUp(){
+
+}
+
+export async function LogOut(){
+
+}
+
+export async function ResetPassword(){
+    
+}
---
+++ server/src/routes/authRoutes.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from "express";
+
+
+const AuthRouter = Router();
+
+//Appointment Routes:
+
+//Create Appointment Route:
+AuthRouter.post('/auth/login');
+
+AuthRouter.post('/auth/signup');
+
+AuthRouter.post('/auth/logout');
+
+AuthRouter.post('/auth/reset');
